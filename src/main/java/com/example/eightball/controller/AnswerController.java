3db919fe94b43e33e65c8c318833f6ba5bcdfb54
Kalyan Manam <manamkalyan07@gmail.com>
2021-05-21 03:37:48 +0530

    crating the repository

---
+++ src/main/java/com/example/eightball/controller/AnswerController.java
@@ -0,0 +1,12 @@
+package com.example.eightball.controller;
+
+import com.example.eightball.service.AnswerService;
+import lombok.NonNull;
+
+public class AnswerController {
+
+  @NonNull
+  private final AnswerService answerService;
+
+  // TODO: Add an endpoint to return an answer to a question
+}
---
+++ src/main/java/com/example/eightball/service/AnswerService.java
@@ -0,0 +1,21 @@
+package com.example.eightball.service;
+
+import com.example.eightball.configuration.EightBallConfig;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class AnswerService {
+
+  @NonNull
+  private final MongoTemplate mongoTemplate;
+
+  @NonNull
+  private final EightBallConfig eightBallConfig;
+
+  // TODO: Create a service method that returns a random answer with the type
+  //  (e.g. affirmative / negative), and saves the returned answer to the mongo database
+}
